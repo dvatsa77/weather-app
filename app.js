eb88d5b945ecc5ea630b8939bed825459b12edd8
Dharesh Vatsa <dvatsa77@gmail.com>
2021-07-21 12:50:39 +0530

    forecast functionality added

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-const request=require('request')
-const geocode=require('./geocode')
+const request = require('request')
+const geocode = require('./geocode')
+const forecast = require('./forecast')
 
-geocode('New York', (error, data) => {
-    console.log('Error:', error)
-    console.log('Data:', data)
-})
+const address = process.argv[2]
+if (!address) {
+    console.log("Please provide an address")
+}
+else {
+    //first we get the coordinates of the place from the address using the geocode API
+    //then using the coordinates we get the weather forecast at the place using the weatherstack API
+    geocode(address, (error, geocodeData) => {
+        if (error) {
+            return console.log(error)
+        }
+
+        forecast(geocodeData.latitude, geocodeData.longitude, (error, forecastData) => {
+            if (error) {
+                return console.log(error)
+            }
+            console.log(geocodeData.location)
+            console.log(forecastData)
+        })
+    })
+}
+
+
+
 
 
